b7c57730ed69bf6a1a6d68c2084d383363280748
FabRisky <dmitrievich77@gmail.com>
2023-06-03 12:56:02 +0300

    fix modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,7 +1,7 @@
-import {Dispatch, FC, KeyboardEvent, SetStateAction, useCallback, useEffect} from 'react';
+import { Dispatch, FC, KeyboardEvent, SetStateAction, useCallback, useEffect } from 'react';
 import ModalOverlay from "../modal-overlay/ModalOverlay";
-import {Burger} from "../../types/types";
-import {CloseIcon} from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons';
+import { Burger } from "../../types/types";
+import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons';
 import styles from './modal.module.scss'
 import IngredientDetails from "../ingredient-details/IngredientDetails";
 import ReactDOM from "react-dom";
@@ -13,60 +13,59 @@ interface IModal {
   currentBurgerIngredient: Burger | null
   setCurrentBurgerIngredient: Dispatch<SetStateAction<Burger | null>>
   setOrder: Dispatch<SetStateAction<boolean>>
-  order:boolean
+  order: boolean
 }
 
 const modalRoot = document.getElementById('modal');
+const ESC_KEYCODE = 27
 
-const Modal: FC<IModal> = ({openModal, setOpenModal,setOrder, order, currentBurgerIngredient, setCurrentBurgerIngredient}) => {
-
-  useEffect(() => {
-    if (currentBurgerIngredient || order) {
-      setOpenModal(true)
-    } else {
-      setOpenModal(false)
-    }
-  }, [currentBurgerIngredient, order])
-
+const Modal: FC<IModal> = ({ openModal, setOpenModal, setOrder, order, currentBurgerIngredient, setCurrentBurgerIngredient }) => {
   const closeModal = useCallback(() => {
     setOpenModal(false)
     setOrder(false)
     setCurrentBurgerIngredient(null)
-  }, [setOpenModal, setCurrentBurgerIngredient])
+  }, [setOpenModal, setCurrentBurgerIngredient, setOrder])
 
   const escHandle = useCallback((e: KeyboardEvent<Document>): void => {
-    if (e.keyCode === 27) {
+    if (e.keyCode === ESC_KEYCODE) {
       closeModal();
     }
   }, [closeModal])
 
   useEffect(() => {
-    if (openModal) {
-      document.addEventListener("keydown", escHandle as any);
-    }
+    document.addEventListener("keydown", escHandle as any);
     return () => {
       document.removeEventListener("keydown", escHandle as any);
     }
-  }, [openModal, escHandle])
+  }, [escHandle])
+
+  useEffect(() => {
+    if (currentBurgerIngredient !== null || order) {
+      setOpenModal(true);
+    } else {
+      setOpenModal(false);
+    }
+  }, [currentBurgerIngredient, order, setOpenModal])
 
   return ReactDOM.createPortal(
-    <>
-      <ModalOverlay openModal={openModal} closeModal={closeModal}/>
-      <div className={!openModal ? styles.modal : `${styles.modal} ${styles.modalOpen}`}>
-        <div
-          className={styles.buttonClose}
-          onClick={closeModal}
-        >
-          <CloseIcon type="primary"/>
+    openModal && (
+      <>
+        <ModalOverlay openModal={openModal} closeModal={closeModal} />
+        <div className={!openModal ? styles.modal : `${styles.modal} ${styles.modalOpen}`}>
+          <div
+            className={styles.buttonClose}
+            onClick={closeModal}
+          >
+            <CloseIcon type="primary" />
+          </div>
+
+          {currentBurgerIngredient && <IngredientDetails currentItem={currentBurgerIngredient} />}
+          {order && <OrderDetails />}
         </div>
-
-        {currentBurgerIngredient && <IngredientDetails currentItem={currentBurgerIngredient}/>}
-        {order && <OrderDetails />}
-
-      </div>
-    </>
-
-    , modalRoot as HTMLElement);
+      </>
+    ),
+    modalRoot as HTMLElement
+  );
 };
 
 export default Modal;
